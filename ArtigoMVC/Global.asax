4950c41aff0d4f58c672188af6535ae79ce4ef7e
ronnioliveira <ronnioliveira@hotmail.com>
2018-05-02 17:49:26 -0300

    Commit do sistema MVC do zero

---
+++ ArtigoMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArtigoMVC.MvcApplication" Language="C#" %>
---
+++ ArtigoMVC/Models/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtigoMVC.Models
+{
+	public class Carro
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Montadora { get; set; }
+		public int Ano { get; set; }
+		public string Modelo { get; set; }
+	}
+}
